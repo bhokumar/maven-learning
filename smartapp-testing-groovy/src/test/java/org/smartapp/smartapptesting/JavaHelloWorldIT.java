86ceb4d2461333210d9ef878c53df0a9a6ce9d34
bhokumar <bhoopendra.akgec@gmail.com>
2019-10-13 07:46:07 +0530

    Updating Surefire plugins version

---
+++ smartapp-testing-groovy/src/test/java/org/smartapp/smartapptesting/JavaHelloWorldIT.java
@@ -0,0 +1,12 @@
+package org.smartapp.smartapptesting;
+
+
+import org.junit.Test;
+
+public class JavaHelloWorldIT {
+
+    @Test
+    public void name() {
+        System.out.println("My IT Ran!");
+    }
+}
